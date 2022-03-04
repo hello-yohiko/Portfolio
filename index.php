7498015c2fd026535e6d187c15aa67f43a22bb28
Yohiko <100919097+hello-yohiko@users.noreply.github.com>
2022-03-04 23:12:24 +0100

    0.0.4

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dévelopeuse Free & Ouvert - HIMI</title>
+	<!-- styles css -->
+	<link rel="stylesheet" type="text/css" href="asset/a.css">
+	<link rel="stylesheet" type="text/css" href="asset/screenup.css">
+	<!-- font CSS -->
+	<link href="http://fonts.cdnfonts.com/css/kulon" rel="stylesheet">
+	<link href="http://fonts.cdnfonts.com/css/alexandriaflf" rel="stylesheet">
+	<link href="http://fonts.cdnfonts.com/css/roboto" rel="stylesheet">
+	<!-- Clear UPDATE WEBSITE -->
+	<script>document.write('<script src="http://' + (location.host || '127.0.0.1').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<!-- Clear UPDATE WEBSITE -->
+	<!-- TWITTER EMDED -->
+	<meta property="og:keywords" content="Informatique, blog, information, service, developement, langage, aide, autodidtact">
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="og:type" content="portfolio">
+    <meta property="og:site_name" content="Hely Emma">
+    <meta property="og:title" content="Dévelopeuse Free & Ouvert - HIMI">
+    <meta property="og:description" content="développeuse Ouverte et Gratuite, à tous et projets">
+    <meta property="og:image" content="/workplace-pink.jpg">
+    <meta property="twitter:image" content="/workplace-pink.jpg">
+    <!-- END TWITER EMBED -->
+</head>
+<body>
+	<div id="root" class="block auto-scroll rooter-contenaire">
+		
+		<section class="main-home">
+			
+			<div class="grid banner">
+				
+				<div class="gird-ele ela">
+					
+					<img src="asset/cat.jpg" width="100%">
+
+				</div>
+				<div class="gird-ele elb">
+					<div class="content-ele">
+						
+						<div class="text big">
+							<p>Are you looking for a free and open person to carry out your projects?</p>
+							<h3>Vous recherchez une personne libre et ouverte pour réaliser vos projet ?</h3>
+						</div>
+						<div class="text litle bio">
+							<p class="para">
+								Je m'appelle Inès, je suis autodidtact en Language de dévelopement informatique. J'ai commcé à étudié à ~12ans. Aujourd'hui, j'ai choisis de faire partager ce que j'ai aprris en proposant de développer votre projet. J'épsère que vous aidant j'apprendrais d'autres choses par le bié de vos demande et vos projet.
+							</p>
+							<p>
+								je suis étudiante en CAP Agricole - Service à la personne et vantes en rural. J'ai pour embitions de réalisé une entreprise dans l'informatique et l'aides informatiques.
+							</p>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+
+			<section class="border-contute about">
+				
+				<div class="content">
+					
+					<span class="img background-content" style="background-image: url(asset/about_small.png);"></span>
+
+					<div class="content grid">
+						
+						<div class="left-cont">
+							
+							<div>
+								<div class="text big">
+									<h3>Proches, Active et Serviable</h3>
+								</div>
+
+								<div class="text litle">
+									<p>
+										Je m'appelle Inès, je suis autodidtact en Language de dévelopement informatique. J'ai commcé à étudié à ~12ans. Aujourd'hui, j'ai choisis de faire partager ce que j'ai aprris en proposant de développer votre projet. J'épsère que vous aidant j'apprendrais d'autres choses par le bié de vos demande et vos projet.<br>
+										je suis étudiante en CAP Agricole - Service à la personne et vantes en rural. J'ai pour embitions de réalisé une entreprise dans l'informatique et l'aides informatiques.
+									</p>
+								</div>
+							</div>
+
+						</div>
+						<div class="right-cont">
+							<div class="img-content">
+								<img src="asset/workplace-pink.jpg" width="100%">
+							</div>
+
+							<div class="flex center-content">
+								
+								<a href="mailto:hely.emma@sokanes.ga?subject=My request Title&body=My request content" class="btn-cliked">
+									Me Contacter
+								</a>
+
+							</div>
+
+						</div>
+
+
+					</div>
+
+				</div>
+
+			</section>
+			<section class="border-contute"></section>
+			<section class="border-contute"></section>
+			<section class="border-close"></section>
+
+		</section>
+
+	</div>
+</body>
+</html>
